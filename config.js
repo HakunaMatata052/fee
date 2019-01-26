efda25f4261fd6e5663862b6c96052bd642d3906
Twinkleee <365189883@qq.com>
2019-01-26 14:24:14 +0800

    增加配置文件

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+    sql:{
+        host     :  '127.0.0.1',
+        user     :  'root',
+        password :  'root',
+        database :  'test'
+      }
+      
+}
+module.exports =  config
--- get.js
+++ get.js
@@ -1,10 +1,6 @@
 const mysql = require('mysql')
-const pool = mysql.createPool({
-  host     :  '127.0.0.1',
-  user     :  'root',
-  password :  'root',
-  database :  'ludan'
-})
+const config = require('./config.js')
+const pool = mysql.createPool(config.sql)
 
 // 接收一个sql语句 以及所需的values
 // 这里接收第二参数values的原因是可以使用mysql的占位符 '?'
